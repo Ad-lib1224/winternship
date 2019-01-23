8d14801b5588e9789275398ffa2afd6dde6c3a66
Sarah Charles <scharles@kargo.com>
2019-01-23 11:35:27 -0500

    added google charts

--- client/js/CatChart.jsx
+++ client/js/CatChart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from 'axios';
 import * as V from 'victory';
+import { Chart } from "react-google-charts";
 import { Button, Grid, Row, Col } from "react-bootstrap";
 
 
@@ -27,6 +28,25 @@ export default class CatChart extends React.Component {
     render() {
         return (
             <div>
+                <Chart
+                  width={'500px'}
+                  height={'300px'}
+                  chartType="PieChart"
+                  loader={<div>Loading Chart</div>}
+                  data={[
+                    ['Task', 'Hours per Day'],
+                    ['Work', 11],
+                    ['Eat', 2],
+                    ['Commute', 2],
+                    ['Watch TV', 2],
+                    ['Sleep', 7],
+                  ]}
+                  options={{
+                    title: 'My Daily Activities',
+                  }}
+                  rootProps={{ 'data-testid': '1' }}
+                />
+                <hr/>
                 <V.VictoryChart
                     domainPadding={20}
                 >
